1ada2a9b2fc42fb67f7e77e9c1c167a7ff6a9858
Riya Joshi <joshiriya2004@gmail.com>
2026-07-27 22:48:00 +0530

    Time: 0 ms (100%), Space: 87.1 MB (96.88%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+      int five=0, ten=0;
+      for(int x:bills){
+        if(x==5){
+            five++;
+        }
+        else if(x==10){
+            if(five>0){
+                five--;
+                ten++;
+            }
+            else{
+                return false;
+            }
+        }
+        else {
+            if(five>0 && ten>0){
+                five--;
+                ten--;
+            }
+            else if(five >2){
+                five-=3;
+            }
+            else {
+                return false;
+            }
+        }
+      }
+      return true;
+    }
+};
